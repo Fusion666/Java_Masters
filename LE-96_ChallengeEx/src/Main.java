45d07baa851046489dbb616f6c21a8566ae610a7
Npetr0v97 <69013216+Npetr0v97@users.noreply.github.com>
2021-01-03 16:11:52 +0200

    Upload pt1

---
+++ LE-96_ChallengeEx/src/Main.java
@@ -0,0 +1,81 @@
+class Car {
+    private String name;
+    private boolean engine;
+    private int cylinders;
+    private int wheels;
+
+    public Car(String name, int cylinders) {
+        this.name = name;
+        this.engine = true;
+        this.cylinders = cylinders;
+        this.wheels = 4;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getCylinders() {
+        return cylinders;
+    }
+
+    public void startEngine() {
+        System.out.println("Engine started.");
+    }
+
+    public int accelerate(int rateOfAcceleration, int timeOfAcceleration, int maxSpeed) {
+        for(int i = 1; i < timeOfAcceleration; i++) {
+            if(rateOfAcceleration < maxSpeed) {
+                rateOfAcceleration += rateOfAcceleration;
+            }
+            else {
+                System.out.println("Maximum speed reached at: " + i + " second");
+                return maxSpeed;
+            }
+        }
+        return rateOfAcceleration;
+    }
+
+    public void brake() {
+        System.out.println("Brake pulled.");
+    }
+}
+
+class Dodge extends Car {
+    public Dodge() {
+        super("Viper", 8);
+    }
+
+    @Override
+    public void brake() {
+        System.out.println("Dodge brake pulled.");
+    }
+}
+
+class Porsche extends Car {
+    public Porsche() {
+        super("Panamera", 8);
+    }
+
+    @Override
+    public void brake() {
+        System.out.println("Porsche brake pulled.");
+    }
+}
+
+class Tesla extends Car {
+    public Tesla() {
+        super("X", 0);
+    }
+
+    @Override
+    public void brake() {
+        System.out.println("Tesla brake pulled.");
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
